7705726740b1e920861963cc18d21ade51ee8003
Matt Roumeliotis <50805718+MattRoumo27@users.noreply.github.com>
2021-01-14 18:17:13 -0500

    Finished the tutorial online

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
@@ -8,3 +9,18 @@ var server = app.listen(4000, function() {
 
 // Static files
 app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+    console.log('Made socket connection', socket.id);
+
+    socket.on('chat', function(data) {
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data) {
+        socket.broadcast.emit('typing', data);
+    });
+});
