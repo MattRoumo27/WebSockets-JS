b183f396dc9011c3dfeb9ae0bfc61960d9ba6573
Matt Roumeliotis <50805718+MattRoumo27@users.noreply.github.com>
2021-02-06 14:16:54 -0500

    Allow private messages to occur between iOS clients

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ io.on('connection', function(socket) {
     socket.on('registerName', function(clientNickname) {
         userList[socket.id] = clientNickname;
         console.log(userList);
+
+        // Send updated list of clients to all the users connected
+        console.log('Sending username list to clients');
+        io.sockets.emit('participantList', userList);
     });
 
     // When the server receives a chat, it will send it to all the clients
@@ -36,8 +40,28 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('typing', data);
     });
 
-    socket.on('participantsList', function() {
-        console.log("Request received to send list");
-        socket.emit('participantList', userList);
+    // Listen for an individual message that is sent by one user to another
+    socket.on('privateChat', function(data) {
+        var msg = data.message;
+        var userSending = data.handle;
+        var userReceiving = data.user;
+        console.log(msg, userSending, userReceiving);
+        for (var key in userList) {
+            if (userList[key] == userReceiving) {
+                // Send private chat to receiving user
+                io.to(key).emit('privateChat', { 
+                    message: msg,
+                    sending: userSending,
+                    receiving: userReceiving
+                });
+
+                // Send chat back to the sending user as well
+                socket.emit('privateChat', {
+                    message: msg,
+                    sending: userSending,
+                    receiving: userReceiving
+                });
+            }
+        }
     });
 });
